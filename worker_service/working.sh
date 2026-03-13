d999719ff32c0b28b6274345e1b15c329ce2c9a7
D OJHA <133635800+DOthedot@users.noreply.github.com>
2026-03-13 17:09:51 +0530

    Delete worker_service/working.sh

--- worker_service/working.sh
+++
@@ -1,6 +0,0 @@
-// to see if the stream processor is working 
-docker logs -f stream-processor
-
-
-// to see the data in the table 
-docker exec scylladb cqlsh -e "SELECT symbol, price, ts FROM stock_pipeline.stock_trades LIMIT 10;"
